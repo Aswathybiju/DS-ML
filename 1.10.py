1c5116b0e93d6edd127d5bfd00b102bb9ca64b9c
Aswathybiju <103183014+Aswathybiju@users.noreply.github.com>
2022-11-29 20:42:10 -1100

    Add files via upload

--- 1.10.py
+++ 1.10.py
@@ -1,6 +1,6 @@
 import numpy as np
-nums1 = np.array([[2, 5, 2], [1, 5, 5]])
-nums2 = np.array([[5, 3, 4], [3, 2,  5]])
+nums1 = np.array([[2, 5, 2],[1, 5, 5]])
+nums2 = np.array([[5, 3, 4],[3, 2, 5]])
 print("Array1:")
 print(nums1)
 print("Array2:")
